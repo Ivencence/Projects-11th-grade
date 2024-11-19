d69e423831271ffe3c3da27d2e8c9d889a1eb00c
IvaPandohova <ipandohova@gmail.com>
2024-11-19 10:45:10 +0200

    Create addressBookExtended.java

---
+++ addressBookExtended.java
@@ -0,0 +1,65 @@
+public class Main {
+    public static void addressBookExtended(String[] args) {
+        // Task 6: Create more objects and test the methods
+        Person p1 = new Person("Eda", "eda@gmail.com", "123-456-7890", 31);
+        p1.print();
+        Person p2 = new Person("Hannah", "hannah@gmail.com", "404-899-9955", 20);
+        p2.print();
+        Person p3 = new Person("Iva", "ipandohova@gmail.com", "57934784", 17);
+        p3.print();
+
+
+        // Task 8: Test the updateEmail method
+
+    }
+}
+
+// Define the Person class here
+class Person {
+    // Instance variables
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private int age;
+
+    // Task 1: Add a new attribute (e.g., age)
+
+    // Constructor: Initialize Person data
+    public Person(String initName, String initEmail, String initPhone, int initAge) { // Task 2: modify method signature to include your new attribute.
+        name = initName;
+        email = initEmail;
+        phoneNumber = initPhone;
+        age = initAge;
+
+        // Task 3: Assign a default value to the new attribute here.
+    }
+
+    public void print() {
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Age: " + age);
+
+        // Task 4: Print the new attribute here
+    }
+
+    // Task 5: Add getter methods to access and return each attribute individually. Here's an example:
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+        // Task 7: Add a method to update the email
+
+}
